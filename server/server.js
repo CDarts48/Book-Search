2c8573d8e67e373c4394fd1a6c3ab65f699cd91e
Corey Donahue <cdartswebdev@gmail.com>
2024-04-01 21:16:46 -0600

    added the authmiddleware to server.js and changed the name of the app to readrader in connection.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3002;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => {
+    // Call our authMiddleware function with the incoming request
+    const authUser = authMiddleware(req);
+
+    // Add the user to the context
+    return { user: authUser };
+  },
 });
 
 app.use(cors());
@@ -22,9 +29,7 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  server.applyMiddleware({ app, path: '/graphql' });
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -37,10 +42,10 @@ const startApolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
