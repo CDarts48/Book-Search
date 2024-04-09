58bafb2ede7af931fe5278408c50abd5ca33d82c
Corey Donahue <cdartswebdev@gmail.com>
2024-04-09 13:17:36 -0600

    added app.use(express.static(path.join(__dirname, 'client/dist'))); to line 31 server.js

--- server/server.js
+++ server/server.js
@@ -31,8 +31,7 @@ const startApolloServer = async () => {
   });
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
+    app.use(express.static(path.join(__dirname, 'client/dist')));
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
