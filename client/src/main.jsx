3e29544110ab7671b45ab4154554657730e84625
Corey Donahue <cdartswebdev@gmail.com>
2024-04-09 20:17:16 -0600

    night 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,28 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import App from './App.jsx';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import App from './App.jsx'
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
 
-const root = document.getElementById('root');
-ReactDOM.createRoot(root).render(
-  <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={SearchBooks} />
-        <Route path="/saved" component={SavedBooks} />
-      </Switch>
-    </App>
-  </Router>
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />
+      }, {
+        path: '/saved',
+        element: <SavedBooks />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
