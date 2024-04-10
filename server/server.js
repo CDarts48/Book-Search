29dc17f79a8908745fccc58a1c5724a4d671c305
Corey Donahue <cdartswebdev@gmail.com>
2024-04-10 10:40:35 -0600

    adjusted wildcard rout for deployment app.git line 35

--- server/server.js
+++ server/server.js
@@ -32,8 +32,9 @@ const startApolloServer = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client')));
-        app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/index.html'));    });
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
   }
 
   db.once('open', () => {
