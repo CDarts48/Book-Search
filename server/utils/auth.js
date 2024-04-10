68338d2b0ef9786cf844dc562b85220b2ad1c49d
Corey Donahue <cdartswebdev@gmail.com>
2024-04-10 13:47:04 -0600

    added some console.logs to auth.js to check login sing up functionality

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -16,27 +16,36 @@ module.exports = {
   authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-
+  
+    // Log the received token
+    console.log('Received token:', token);
+  
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+  
+    // Log the processed token
+    console.log('Processed token:', token);
+  
     if (!token) {
+      console.log('No token provided');
       return req;
     }
-
+  
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
+      console.log('User data from token:', data);
+    } catch (err) {
+      console.log('Invalid token:', err);
     }
-
+  
     // send to next endpoint
-   return req;
+    return req;
   },
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
